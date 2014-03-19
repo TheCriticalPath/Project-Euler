11b2e5b9c2551e774b06fd468e19dc47833e4bef
TheCriticalPath <Tony.J.Clark@gmail.com>
2014-03-19 16:54:54 -0400

    Problem 16

---
+++ Plugins/Problem 16/plugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+using System.Numerics;
+namespace Problem_16
+{
+    [Export(typeof(IEulerPlugin))]
+    public class EulerPlugin : IEulerPlugin
+    {
+        public long _limit;
+        public bool ImplementsGetInput { get { return true; } }
+        public int ID { get { return 16; } }
+        public string Name
+        {
+            get { return string.Format("Problem {0}: {1}", ID, Title); }
+        }
+        public string Title { get { return string.Format("Power digit sum", ID); } }
+        public string Description { get { return "What is the sum of the digits of the number 2^x?"; } }
+        public EulerPlugin() { }
+
+        private long GetLimit()
+        {
+            long lngLimit = 0;
+            string strLimit = "1000";
+
+            while (lngLimit < 1)
+            {
+                Helpers.InputBox.Show(Name, "Enter value for x", ref strLimit);
+                if (!Int64.TryParse(strLimit, out lngLimit))
+                {
+                    lngLimit = 0;
+                }
+
+            }
+            return lngLimit;
+        }
+        public void PerformGetInput(IEulerPluginContext context)
+        {
+            _limit = GetLimit();
+        }
+        public IEulerPluginContext PerformAction(IEulerPluginContext context)
+        {
+
+            context.strResultLongText = PowerDigitsSum((int)_limit);
+            return context;
+        }
+        public string PowerDigitsSum(int power) {
+            BigInteger x = BigInteger.Pow(2, power);
+
+            String s = x.ToString("F0");
+            int v = 0;
+            for (int i = 0; i < s.Length; i++) {
+                v += int.Parse(s[i].ToString());
+            }
+            return string.Format("The sum of the digits in 2^{0} is {1}", power, v);
+        }
+
+    }
+}
