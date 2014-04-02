fcff74df59a9fc6bd173246dd4b9f48afa487796
TheCriticalPath <Tony.J.Clark@gmail.com>
2014-04-02 17:52:09 -0400

    Problem 35

--- Helpers/Helpers/StringHelper.cs
+++ Helpers/Helpers/StringHelper.cs
@@ -8,12 +8,46 @@ namespace Helpers
 {
     public static class StringHelper
     {
-        public static string JoinInt(this int[] array) {
+        public static string JoinInt(this int[] array)
+        {
             StringBuilder retval = new StringBuilder();
-            for (int i = 0; i < array.Length; i++) {
+            for (int i = 0; i < array.Length; i++)
+            {
                 retval.AppendFormat("{0},", array[i]);
             }
             return retval.ToString(0, retval.Length - 1);
-         }
+        }
+        public enum SHIFTDIRECTION
+        {
+            SHIFTLEFT = 1,
+            SHIFTRIGHT = 2,
+            SHIFTLEFT_CIRCULAR = 4,
+            SHIFTRIGHT_CIRCULAR = 8
+        }
+        public static string Shift(this string s, SHIFTDIRECTION d)
+        {
+            string r = string.Empty;
+            if (s.Length < 2) return s;
+            switch (d)
+            {
+                case SHIFTDIRECTION.SHIFTLEFT:
+                    r = s.Remove(0, 1);
+                    break;
+                case SHIFTDIRECTION.SHIFTRIGHT:
+                    r = s.Remove(s.Length - 1, 1);
+                    break;
+                case SHIFTDIRECTION.SHIFTLEFT_CIRCULAR:
+                    r = string.Format("{0}{1}", s.Substring(1, s.Length - 1), s.Substring(0, 1));
+                    break;
+                case SHIFTDIRECTION.SHIFTRIGHT_CIRCULAR:
+                    r = string.Format("{0}{1}", s.Substring(s.Length-1,1),s.Substring(0, s.Length - 1));
+                    break;
+                default:
+                    break;
+            }
+            return r;
+        }
+       
+
     }
 }
--- Plugins/Problem 35/plugin.cs
+++ Plugins/Problem 35/plugin.cs
@@ -20,14 +20,24 @@ namespace Problem_35
         {
             get { return string.Format("Problem {0}: {1}", ID, Title); }
         }
-        public string Title { get { return string.Format("Project Name", ID); } }
-        public string Description { get { return @"Project Desc"; } }
+        public string Title { get { return string.Format("Circular Primes", ID); } }
+        public string Description
+        {
+            get
+            {
+                return @"The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?";
+            }
+        }
         public EulerPlugin() { }
 
         private long GetLimit()
         {
             long lngLimit = 0;
-            string strLimit = "1000";
+            string strLimit = "1000000";
 
             while (lngLimit < 1)
             {
@@ -46,8 +56,54 @@ namespace Problem_35
         }
         public IEulerPluginContext PerformAction(IEulerPluginContext context)
         {
-            context.strResultLongText = "";
+            context.strResultLongText = BruteForce(_limit);
             return context;
+        }
+        List<int> Primes = new List<int>();
+        public string BruteForce(long limit) { 
+            int count = 0; //There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+            Primes.Add(1); count++;
+            Primes.Add(2); count++; Primes.Add(3); count++; Primes.Add(5); count++;
+            for (int i = 7; i < limit;i+=2){
+                if (!Primes.Contains(i)) {
+                    if (MathHelper.IsPrime(i))
+                    {
+                        if (i.ToString().IndexOfAny(new char[] { '0', '2', '4', '5', '6', '8', '0' }) < 0)
+                        {
+                            Primes.Add(i);
+                        }
+                    }
+                }   
+            }
+            foreach (int p in Primes) {
+                bool all = true;
+                if (p > 10)
+                {
+                    string s = p.ToString().Shift(StringHelper.SHIFTDIRECTION.SHIFTLEFT_CIRCULAR);
+                    do
+                    {
+                        if (!Primes.Contains(int.Parse(s)))
+                        {
+                            all = false;
+                            break;
+                        }
+                        s = s.Shift(StringHelper.SHIFTDIRECTION.SHIFTLEFT_CIRCULAR);
+                    } while (s != p.ToString());
+                    if (all)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+
+
+            return string.Format("There are {0} circular primes below one million", count);
+        }
+        private int RotateNumber(int i) {
+            int r = i;
+            string s = i.ToString();
+            return int.Parse(s.Shift(StringHelper.SHIFTDIRECTION.SHIFTLEFT_CIRCULAR));
         }
 
     }
