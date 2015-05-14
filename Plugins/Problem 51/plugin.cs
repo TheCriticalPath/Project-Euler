477c9d63857b49575bda9be450a8486889bd9b5d
TheCriticalPath <Tony.J.Clark@gmail.com>
2015-05-14 10:57:45 -0400

    P51 Shell

---
+++ Plugins/Problem 51/plugin.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+using Helpers;
+using System.Numerics;
+namespace Problem_51
+{
+    [Export(typeof(IEulerPlugin))]
+    public class EulerPlugin : IEulerPlugin
+    {
+        public bool IsAsync { get { return true; } }
+        public bool ImplementsGetInput { get { return true; } }
+
+        public long _upperLimit;
+        public long _lowerLimit;
+        public int ID { get { return 51; } }
+        public string Name
+        {
+            get { return string.Format("Problem {0}: {1}", ID, Title); }
+        }
+        public string Title { get { return string.Format("Prime digit replacements", ID); } }
+        public string Description
+        {
+            get
+            {
+                return @"By replacing the 1st digit of the 2-digit number *3, it turns out that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit number is the first example having seven primes among the ten generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993. Consequently 56003, being the first member of this family, is the smallest prime with this property.
+Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.";
+            }
+        }
+        public EulerPlugin() { }
+
+        private long GetLimit(string strModifier = "", string defaultLimit = "1000")
+        {
+            long lngLimit = 0;
+            string strLimit = defaultLimit;
+
+            while (lngLimit < 1)
+            {
+                Helpers.InputHelper.Show(Name, string.Format("Enter {0} Limit", strModifier), ref strLimit);
+                if (!Int64.TryParse(strLimit, out lngLimit))
+                {
+                    lngLimit = 0;
+                }
+
+            }
+            return lngLimit;
+        }
+        public void PerformGetInput(IEulerPluginContext context)
+        {
+            _lowerLimit = GetLimit("Lower", "286");
+            _upperLimit = GetLimit("Upper", "2000");
+        }
+
+        public Task<IEulerPluginContext> PerformActionAsync(IEulerPluginContext context)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                // need a more elegant solution.
+                DateTime dtStart, dtEnd;
+                dtStart = DateTime.Now;
+                Task<String> s = BruteForceAsync();
+                dtEnd = DateTime.Now;
+                context.strResultLongText = s.Result;
+                context.spnDuration = dtEnd.Subtract(dtStart);
+                return context;
+            });
+        }
+
+        public IEulerPluginContext PerformAction(IEulerPluginContext context)
+        {
+            context.strResultLongText = "";
+            return context;
+        }
+        async Task<string> BruteForceAsync()
+        {
+            //return Brute2();
+            return BruteForce();
+        }
+        private string BruteForce()
+        {
+            string retval = string.Empty;
+
+            return retval;
+        }
+    }
+}
